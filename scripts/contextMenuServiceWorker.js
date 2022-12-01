77b87e3f476711defa2d595cd6b05a2af8489105
headcheck <rob@headcheck.net>
2022-12-01 06:17:18 -0800

    Initial commit

---
+++ scripts/contextMenuServiceWorker.js
@@ -0,0 +1,80 @@
+const getKey = () => {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(['openai-key'], (result) => {
+            if (result['openai-key']) {
+                const decodedKey = atob(result['openai-key']);
+                resolve(decodedKey);
+            }
+        });
+    });
+};
+
+const sendMessage = (content) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0].id;
+
+        chrome.tabs.sendMessage(
+                activeTab,
+                { message: 'inject', content },
+                (response) => {
+                    if (response.status === 'failed') {
+                        console.log('injection failed.');
+                    }
+                }
+                );
+    });
+};
+
+const generate = async (prompt) => {
+    // Get your API key from storage
+    const key = await getKey();
+    const url = 'https://api.openai.com/v1/completions';
+
+    // Call completions endpoint
+    const completionResponse = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${key}`,
+        },
+        body: JSON.stringify({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            max_tokens: 1250,
+            temperature: 0.7,
+        }),
+    });
+
+    // Select the top choice and send back
+    const completion = await completionResponse.json();
+    return completion.choices.pop();
+}
+
+const generateCompletionAction = async (info) => {
+    try {
+        sendMessage('generating...');
+
+        const { selectionText } = info;
+        const basePromptPrefix = `
+        List the largest 10 campaign donors for the politician list below. How much did the donor contribute? What industry does the donor represent?
+
+        politician:
+        `;
+
+        const completion = await generate(`${basePromptPrefix}${selectionText}`);
+
+        sendMessage(completion.text);
+    } catch (error) {
+        console.log(error);
+        sendMessage(error.toString());
+    }
+};
+
+chrome.contextMenus.create({
+    id: 'context-run',
+    title: 'Generate donor list',
+    contexts: ['selection'],
+});
+
+// Add listener
+chrome.contextMenus.onClicked.addListener(generateCompletionAction);
